b7553de12c21e80a27aeb091279d6c709a47691d
Rimma1704 <106549350+Rimma1704@users.noreply.github.com>
2022-06-02 10:25:27 +0300

    update

---
+++ Program_Sincretic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+//se declara variabilele numar,divizor,putere de tip intreg
+//variabila numar memoreaza numarul citit de la tastatura
+//variabila divizor este folosita pentru a identifica daca numar are divizori proprii
+//variabila putere memoreaza puterea la care apare un factor prim
+int numar, divizor, putere;
